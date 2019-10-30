47c7b33167af8468ceb958bbd5cef44b8524df99
Anders Jensen <johndoee@tidalstream.org>
2019-10-30 18:55:34 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 
 setup(name='malparser',
-      version='1.1.4',
+      version='1.1.5',
       description='Python package to access the MyAnimeList Anime',
       long_description=open('README.rst').read(),
       author='Anders Jensen',
